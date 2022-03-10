b72e7be249d6f5a1482369cec1e8ad8d4ac14fcb
shan793 <sgoonatilaka09@gmail.com>
2022-03-10 11:58:31 -0500

    home page class created and tested

--- src/test/java/deltaTests/testNGRelated/tests/TestSuiteOne.java
+++ src/test/java/deltaTests/testNGRelated/tests/TestSuiteOne.java
@@ -1,6 +1,7 @@
 package deltaTests.testNGRelated.tests;
 
 import deltaTests.Initializer;
+import deltaTests.pageObjectClasses.HomePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,22 +13,27 @@ import org.testng.annotations.Test;
 
 public class TestSuiteOne {
 	private WebDriver driver;
+	private HomePage homepage;
 
 	@BeforeMethod
 	public void initializeWebdriver(){
 		Initializer initializer = new Initializer(); //someUpdate
 		initializer.createChromeDriverInstance();
 		driver = initializer.getDriver();
+		homepage = new HomePage(driver);
 	}
 
 	@Test
 	public void openLoginPage(){
 		driver.get("http://delta.com");
-		WebElement loginButton = driver.findElement(By.xpath("//button[contains(@class, 'login')]"));
-		Actions actions = new Actions(driver);
-		Assert.assertTrue(loginButton.isDisplayed());
-		driver.findElement(By.xpath("//div[contains(@class, 'card-img-overlay')]")).click();
-		actions.moveToElement(loginButton).click().build().perform();
+//		WebElement loginButton = driver.findElement(By.xpath("//button[contains(@class, 'login')]"));
+//		Actions actions = new Actions(driver);
+//		Assert.assertTrue(loginButton.isDisplayed());
+//		driver.findElement(By.xpath("//div[contains(@class, 'card-img-overlay')]")).click();
+//		actions.moveToElement(loginButton).click().build().perform();
+		Assert.assertTrue(homepage.getLoginButtonOnHomePage().isDisplayed());
+		homepage.getOverlayImageAdvertisement().click();
+		homepage.getLoginButtonOnHomePage().click();
 
 		Assert.assertTrue(driver.findElement(By.xpath("//h1[contains(text(), 'To Delta')]")).isDisplayed());
 
