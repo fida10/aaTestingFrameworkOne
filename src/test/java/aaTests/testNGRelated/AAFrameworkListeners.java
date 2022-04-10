8b266666c26d5cb7360232471138cca38bb62868
shan793 <sgoonatilaka09@gmail.com>
2022-04-10 19:05:14 -0400

    Added new page object and validated search figures

--- src/test/java/aaTests/testNGRelated/AAFrameworkListeners.java
+++ src/test/java/aaTests/testNGRelated/AAFrameworkListeners.java
@@ -4,7 +4,7 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class DeltaFrameworkListeners implements ITestListener {
+public class AAFrameworkListeners implements ITestListener {
     @Override
     public void onTestStart(ITestResult result) {
         System.out.println("**********************HELLO EVERYONE TESTING**********************");
